517c4ce9bf81c157089eb105283ea00f437fe2d7
Diego Adilson Vieira <diegoavieira@gmail.com>
2020-06-08 11:35:17 -0300

    build: adjustments

--- projects/web-app/src/app/core/auth/auth.service.ts
+++ projects/web-app/src/app/core/auth/auth.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { environment } from '@enviroment';
+import createAuth0Client from '@auth0/auth0-spa-js';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(private router: Router) {}
+  auth0: any;
+
+  constructor() {}
+
+  async init() {
+    this.auth0 = await createAuth0Client({
+      domain: 'adsystem.auth0.com',
+      client_id: 'v2FQKCJwDxkPPit74TWnvwMpAd5ef9tF'
+    });
+  }
 }
